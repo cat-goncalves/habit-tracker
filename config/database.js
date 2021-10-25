172efd9091093fc9b14149b719088fc0211a96dc
Cat Goncalves <catgoncalves.dev@gmail.com>
2021-10-25 10:47:07 -0400

    Updated config

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://test:test@cluster0.me8ts.mongodb.net/habit-tracker?retryWrites=true&w=majority', 
+    'dbName': 'habit-tracker'
+};
